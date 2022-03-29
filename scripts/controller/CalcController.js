d28ed7b3df81e496e0d16f43e229a5164101c8ed
DidoReis <101334003+DidoReis@users.noreply.github.com>
2022-03-29 10:27:39 -0300

    Update CalcController.js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor(){
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -9,28 +10,127 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
-        
+
     }
 
     initialize(){
 
-            setInterval(() =>{
+        this.setDisplayDateTime()
 
-              this.setDisplayDateTime();
-               
-            }, 1000);
+        setInterval(()=>{
+
+            this.setDisplayDateTime();
+
+        }, 1000);
+
+    }
+
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        })
+    
+    }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
         
+    }
+
+    execBtn(value){
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+                
+                break;
+
+            case 'subtracao':
+                
+                break;
+
+            case 'divisao':
+                
+                break;
+
+            case 'porcento':
+                
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
         }
 
-         initButtonsEvents(){
+    }
+
+    initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index)=>{
 
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn, "click drag", e => {
 
-                console.log(btn.className.baseVal.replace("btn-",""));
+                let textBtn = btn.className.baseVal.replace("btn-","");
+
+                this.execBtn(textBtn);
+
+            })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
 
             })
 
@@ -38,33 +138,39 @@ class CalcController {
 
     }
 
-    setDisplayDateTime (){
+    setDisplayDateTime(){
 
-          this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
             month: "long",
             year: "numeric"
-          });
-          this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
-           
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
     }
 
-    get displayTime() {
+    get displayTime(){
+
         return this._timeEl.innerHTML;
+
     }
 
-    set displayTime(value) {
+    set displayTime(value){
+
         return this._timeEl.innerHTML = value;
+
     }
 
-    get displayDate() {
+    get displayDate(){
 
-        return this._dateEl.innerHTML;    
+        return this._dateEl.innerHTML;
+
     }
 
-    set displayDate(value) {
+    set displayDate(value){
 
-        return this._dateEl.innerHTML = value;    
+        return this._dateEl.innerHTML = value;
+
     }
 
     get displayCalc(){
@@ -73,9 +179,9 @@ class CalcController {
 
     }
 
-    set displayCalc(valor){
+    set displayCalc(value){
 
-        this._displayCalcEl.innerHTML = valor;
+        this._displayCalcEl.innerHTML = value;
 
     }
 
@@ -85,9 +191,9 @@ class CalcController {
 
     }
 
-    set currentDate(valor){
+    set currentDate(value){
 
-        this._currentDate = valor;
+        this._currentDate = value;
 
     }
 
